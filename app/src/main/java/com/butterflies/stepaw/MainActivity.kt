dfd5a0c5656fd34cea550d161c1e5eb76fe66022
Maneesh <thouti.maneesh43@gmail.com>
2021-11-10 21:16:14 -0800

    fix:user preferences will be stored in sharedprefs and also on backend

--- app/src/main/java/com/butterflies/stepaw/MainActivity.kt
+++ app/src/main/java/com/butterflies/stepaw/MainActivity.kt
@@ -3,6 +3,7 @@ package com.butterflies.stepaw
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_NO
@@ -12,23 +13,31 @@ import com.butterflies.stepaw.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var token:String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_NO)
         setSupportActionBar(findViewById(R.id.my_toolbar))
-        Intent(this,ChartReport::class.java).also {
-            startActivity(it) }
-        val sharedPref = getPreferences(Context.MODE_PRIVATE) ?: return
-        val token = sharedPref.getString("com.butterflies.stepaw.idToken", "invalid")
+        val sharedData=getSharedPreferences("com.butterflies.stepaw",Context.MODE_PRIVATE)
+        val token=sharedData.getString("com.butterflies.stepaw.idToken","invalid")
+        if (token == "invalid") {
+            Intent(this, AuthUIHost::class.java).run { startActivity(this) }
+        }else{
+            if (token != null) {
+                Log.d("sharedtoken",token)
+            }
+        }
+//        Intent(this,ChartReport::class.java).also {
+//            startActivity(it) }
+
 //        if (token == "invalid") {
 //            Intent(this, AuthUIHost::class.java).run { startActivity(this) }
 //        } else {
 //            Intent(this, ChartReport::class.java).run { startActivity(this) }
 //        }
-
+//
     }
-
 
 }
